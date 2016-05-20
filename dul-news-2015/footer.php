04aa0799ef2e9c28b52ad56c2aa9f2d734917e2b
Michael Daul <michael.daul@duke.edu>
2016-05-20 10:17:14 -0400

    syncing latest updates

--- dul-news-2015/footer.php
+++ dul-news-2015/footer.php
@@ -20,6 +20,20 @@
 				<?php do_action( 'twentyfourteen_credits' ); ?>
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 			</div><!-- .site-info -->
+
+			<!-- footer search -->
+			<!--
+			<div id="searchform">
+				<form action="/news-events-exhibits/search-results/" id="cse-search-box">
+					<input type="hidden" name="cx" value="010520721692465143024:76gaailhxhm" />
+					<input type="hidden" name="cof" value="FORID:10" />
+					<input type="hidden" name="ie" value="UTF-8" />
+					<input class="search-input" type="text" name="q" size="20" placeholder="search the blogs..." />
+					<button class="search-submit" name="sa" value="Search" type="submit">Go</button>
+				</form>
+			</div>
+			-->
+
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
 
--- dul-news-2015/includes/library_videos_2015.php
+++ dul-news-2015/includes/library_videos_2015.php
@@ -2,9 +2,16 @@
 
 //require_once('simplepie_video.inc');
 
+// no longer supported (api v2)
+// https://www.youtube.com/rss/user/DukeUnivLibraries/videos.rss
+
+
+//https://www.youtube.com/feeds/videos.xml?channel_id=UCV_WWtC0T_8uPMhYasKyPiQ
+
+//https://www.youtube.com/feeds/videos.xml?user=DukeUnivLibraries
 
 $feed = new SimplePie();
-$feed->set_feed_url('http://www.youtube.com/rss/user/DukeUnivLibraries/videos.rss');
+$feed->set_feed_url('https://www.youtube.com/feeds/videos.xml?channel_id=UCV_WWtC0T_8uPMhYasKyPiQ');
 
 // Run SimplePie.
 $feed->init();
@@ -22,10 +29,13 @@ $feed->handle_content_type();
  
 	<?php
 	foreach ($feed->get_items(0, 5) as $item):
+	
+	$videoID = str_replace("http://www.youtube.com/watch?v=", "", $item->get_permalink()); 
+	
 	?>
 	
 		<li>
-		<div class="thumbnail"><a href="<?php echo $item->get_permalink(); ?>" target="_blank" title="<?php echo $item->get_title(); ?>"><img src="<?php echo 'http://i.ytimg.com/vi/' . substr($item->get_permalink(), 31, -26) . '/default.jpg'; ?>" width="120" height="90" alt="<?php echo $item->get_title(); ?></a>" border="0" /></a></div>
+		<div class="thumbnail"><a href="<?php echo $item->get_permalink(); ?>" target="_blank" title="<?php echo $item->get_title(); ?>"><img src="<?php echo 'https://i.ytimg.com/vi/' . $videoID . '/default.jpg'; ?>" width="120" height="90" alt="<?php echo $item->get_title(); ?></a>" border="0" /></a></div>
 		<div class="title"><a href="<?php echo $item->get_permalink(); ?>" target="_blank" title="<?php echo $item->get_title(); ?>"><?php echo $item->get_title(); ?></a></div>
 		</li>
  
@@ -39,7 +49,7 @@ $feed->handle_content_type();
 		
 		<br />
 		
-		<div class="button"><a href="http://www.youtube.com/user/DukeUnivLibraries" target="_blank">See More Videos &raquo;</a></div>
+		<div class="button"><a href="https://www.youtube.com/user/DukeUnivLibraries" target="_blank">See More Videos &raquo;</a></div>
 	
 	</li>
 
